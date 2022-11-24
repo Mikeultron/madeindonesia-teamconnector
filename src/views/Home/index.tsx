080eedc83cc3d845859b2d3bb1a860e076d04446
Mikhael Esa <56322741+Mikeultron@users.noreply.github.com>
2022-11-24 13:05:37 +0700

    Setup (#1)
    * Setup
    
    * Modify package.json

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,12 @@
+import Button from "atoms/Button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default Home;
